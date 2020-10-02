c341a043488b942d6a4f398de635ea2629882780
prographo <smith@mcf.rocks>
2020-10-02 17:28:51 +0100

    use packed and custom errors

--- src/rejectdups-rust/src/lib.rs
+++ src/rejectdups-rust/src/lib.rs
@@ -1,51 +1,151 @@
 use byteorder::{ByteOrder, LittleEndian};
 use solana_sdk::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, info,
-    program_error::ProgramError, account_info::next_account_info, pubkey::Pubkey,
-    hash::Hasher, 
+    decode_error::DecodeError, program_error::ProgramError, account_info::next_account_info, pubkey::Pubkey,
     sysvar::{self, Sysvar}, rent::Rent,
+    program_pack::{Pack, Sealed},
 };
-use std::mem;
 
+use num_derive::FromPrimitive;
+use thiserror::Error;
 
-pub fn create_address_with_seed(
-    base: &Pubkey,
-    seed: &str,
-    program_id: &Pubkey,
-) -> Pubkey {
-    let mut hasher = Hasher::default();
-    hasher.hashv(&[base.as_ref(), seed.as_ref(), program_id.as_ref()]);
-    Pubkey::new( hasher.result().as_ref() )
+#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+pub enum VoteError {
+    #[error("Unexpected Candidate")]
+    UnexpectedCandidate,
+    #[error("Incorrect Owner")]
+    IncorrectOwner,
+    #[error("Account Not Rent Exempt")]
+    AccountNotRentExempt,
+    #[error("Account Not Check Account")]
+    AccountNotCheckAccount,
+    #[error("Already Voted")]
+    AlreadyVoted,
 }
+impl From<VoteError> for ProgramError {
+    fn from(e: VoteError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
+impl<T> DecodeError<T> for VoteError {
+    fn type_of() -> &'static str {
+        "Vote Error"
+    }
+}
+
+// Instruction data
+
+pub struct Vote {
+    pub candidate: u8,
+}
+
+impl Sealed for Vote {}
+
+impl Pack for Vote {
+
+    const LEN: usize = 1;
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        if src.len() != 1 {
+            info!("Instruction data must be 1 byte"); 
+            return Err(ProgramError::InvalidInstructionData);
+        }
+
+        let candidate = src[0];
+
+        if candidate != 1 || candidate != 2 {
+            info!("Vote must be for candidate 1 or 2");
+            return Err(VoteError::UnexpectedCandidate.into());
+        } 
+        Ok(Vote {
+            candidate,
+        })
+    }
+
+    fn pack_into_slice(&self, _dst: &mut [u8]) {}
+}
+
+
+ 
+
+// Vote Check structure, which is one 4 byte u32 number
+// contains zero if they havn't voted, or the candidate number if they have
+
+pub struct VoteCheck {
+    pub voted_for: u32,
+}
+
+impl Sealed for VoteCheck {}
+
+impl Pack for VoteCheck {
+
+    const LEN: usize = 4;
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        Ok(VoteCheck {
+            voted_for: LittleEndian::read_u32(&src[0..4]),
+        })
+    }
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        LittleEndian::write_u32(&mut dst[0..4], self.voted_for);
+    }
+}
+
+
+// Vote Count structure, which is two 4 byte u32 numbers
+// first number is candidate 1's vote count, second number is candidate 2's vote count
+
+pub struct VoteCount {
+  pub candidate1: u32,
+  pub candidate2: u32,
+}
+
+impl Sealed for VoteCount {}
+
+impl Pack for VoteCount {
+
+    const LEN: usize = 8;
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        Ok(VoteCount {
+            candidate1: LittleEndian::read_u32(&src[0..4]),
+            candidate2: LittleEndian::read_u32(&src[4..8]),
+        })
+    }
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        LittleEndian::write_u32(&mut dst[0..4], self.candidate1);
+        LittleEndian::write_u32(&mut dst[4..8], self.candidate2);
+    }
+}
+
 
 // Declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
 // Program entrypoint's implementation
-fn process_instruction<'a>(
+fn process_instruction(
     program_id: &Pubkey,             // Public key of program account
-    accounts: &'a [AccountInfo<'a>], // data accounts
+    accounts: &[AccountInfo],        // data accounts
     instruction_data: &[u8],         // 1 = vote for A, 2 = vote for B
 ) -> ProgramResult {
 
     info!("Rust program entrypoint");
 
+    // get candidate to vote for from instruction_data
+    let candidate = Vote::unpack_unchecked(&instruction_data)?.candidate;
+
     // Iterating accounts is safer then indexing
     let accounts_iter = &mut accounts.iter();
 
     // Get the account that holds the vote count
-    let account = next_account_info(accounts_iter)?;
+    let count_account = next_account_info(accounts_iter)?;
 
     // The account must be owned by the program in order to modify its data
-    if account.owner != program_id {
-        info!("Vote account does not have the correct program id");
-        return Err(ProgramError::IncorrectProgramId);
-    }
-
-    // The data must be large enough to hold two u32 vote counts
-    if account.try_data_len()? < 2 * mem::size_of::<u32>() {
-        info!("Vote account data length too small for u32");
-        return Err(ProgramError::InvalidAccountData);
+    if count_account.owner != program_id {
+        info!("Vote count account not owned by program");
+        return Err(VoteError::IncorrectOwner.into());
     }
 
     // Get the account that checks for dups
@@ -53,14 +153,8 @@ fn process_instruction<'a>(
 
     // The check account must be owned by the program in order to modify its data
     if check_account.owner != program_id {
-        info!("Check account does not have the correct program id");
-        return Err(ProgramError::IncorrectProgramId);
-    }
-
-    // The data must be large enough to hold one u32 int
-    if check_account.try_data_len()? < mem::size_of::<u32>() {
-        info!("Check account data length too small for u32");
-        return Err(ProgramError::InvalidAccountData);
+        info!("Check account not owned by program");
+        return Err(VoteError::IncorrectOwner.into());
     }
 
     // The account must be rent exempt, i.e. live forever
@@ -72,54 +166,63 @@ fn process_instruction<'a>(
     }
     if ! rent.is_exempt(check_account.lamports(),check_account.data_len()) {
         info!("Check account is not rent exempt");
-        return Err(ProgramError::InvalidAccountData);
+        return Err(VoteError::AccountNotRentExempt.into());
     }
 
     // the voter 
     let voter_account = next_account_info(accounts_iter)?;
 
     if !voter_account.is_signer {
+      info!("Voter account is not signer");
       return Err(ProgramError::MissingRequiredSignature);
     }
 
-    let expected_check_account_address = create_address_with_seed(voter_account.key, 
+    let expected_check_account_address = Pubkey::create_with_seed(voter_account.key, 
                                                                   "checkvote", 
                                                                   program_id);
 
-    if expected_check_account_address != *check_account.key {
+    if expected_check_account_address != Ok(*check_account.key) {
         info!("Voter fraud! not the correct check_account.");
-        return Err(ProgramError::InvalidAccountData);
+        return Err(VoteError::AccountNotCheckAccount.into());
     }
 
-    let mut checkdata = check_account.try_borrow_mut_data()?;
+    let mut check_data = check_account.try_borrow_mut_data()?;
 
-    let cac = LittleEndian::read_u32(&checkdata[0..4]);
+    // this unpack reads and deserialises the account data and also checks the data is the correct length
 
-    if cac!=0 {
+    let mut vote_check = VoteCheck::unpack_unchecked(&check_data).expect("Failed to read VoteCheck");
+
+    if vote_check.voted_for != 0 {
         info!("Voter fraud! You already voted.");
-        return Err(ProgramError::InvalidAccountData);
+        return Err(VoteError::AlreadyVoted.into());
     }
 
 
-    // Increment vote count of candidate
+    // Increment vote count of candidate, and record voter's choice
 
-    let mut data = account.try_borrow_mut_data()?;
+    let mut count_data = count_account.try_borrow_mut_data()?;
 
-    if 1 == instruction_data[0] { 
-        let mut vc = LittleEndian::read_u32(&data[0..4]);
-        vc += 1;
-        LittleEndian::write_u32(&mut data[0..4], vc);
-        info!("Voted for candidate1!");
-        LittleEndian::write_u32(&mut checkdata[0..4], 1);
+    let mut vote_count = VoteCount::unpack_unchecked(&count_data).expect("Failed to read VoteCount"); 
+
+    match candidate {
+        1 => {
+            vote_count.candidate1 += 1;
+            vote_check.voted_for = 1; 
+            info!("Voting for candidate1!");
+        },
+        2 => {
+            vote_count.candidate2 += 1;
+            vote_check.voted_for = 2; 
+            info!("Voting for candidate2!");
+        }
+        _ => {
+            info!("Unknown candidate");
+            return Err(ProgramError::InvalidInstructionData);
+        }
     }
 
-    if 2 == instruction_data[0] { 
-        let mut vc = LittleEndian::read_u32(&data[4..8]);
-        vc += 1;
-        LittleEndian::write_u32(&mut data[4..8], vc);
-        info!("Voted for candidate2!");
-        LittleEndian::write_u32(&mut checkdata[0..4], 2);
-    }
+    VoteCount::pack(vote_count, &mut count_data).expect("Failed to write VoteCount");
+    VoteCheck::pack(vote_check, &mut check_data).expect("Failed to write VoteCheck");
 
     Ok(())
 }
@@ -208,7 +311,7 @@ mod test {
 
         // mock voter check_account 
 
-        let key = create_address_with_seed(voter_account.key, "checkvote", &program_id);  // derived (correctly)
+        let key = Pubkey::create_with_seed(voter_account.key, "checkvote", &program_id);  // derived (correctly)
         let mut lamports = 0;
         let mut data = vec![0; mem::size_of::<u32>()];
         LittleEndian::write_u32(&mut data[0..4], 0); // set storage to zero
